9256a4f0498a45207b4a1541623bcf8f80ad696e
Sai Tippana <saiT1108>
2021-11-27 01:23:15 -0500

    finished project

--- project1.py
+++ project1.py
@@ -268,6 +268,10 @@ def lyricsPage():
 def feedPage():
     return flask.render_template("feedback.html")
 
+@app.route("/about")
+def about():
+    return flask.render_template("about.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8000)), debug=True)
